e67b9ed84c274ebd6c80389f56ceb3cc90b86c86
euuhebert <hebertsrocha79@gmail.com>
2023-10-03 20:11:17 -0300

    versão final

--- src/Contador.java
+++ src/Contador.java
@@ -1,26 +1,35 @@
 import java.util.Scanner;
 
-public class Contador {
+public class Contador  {
     public static void main(String[] args) {
-		Scanner terminal = new Scanner(System.in);
+		
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.??;
+		int parametroUm = sc.nextInt();
 		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.??;
+		int parametroDois = sc.nextInt();;
 		
 		try {
 			//chamando o método contendo a lógica de contagem
+			
 			contar(parametroUm, parametroDois);
 		
-		}catch (? exception) {
+		}catch (ParametrosInvalidosException e) {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+			System.out.println(e.getMessage());
 		}
 		
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		if (parametroUm > parametroDois) {
+			throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+
+		}
 		
 		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
+		for ( int i = 1; i <=contagem; i++) {
+            System.out.println("Imprimindo numero " + i);
+        }
 	}
 }
--- src/ParametrosInvalidosException.java
+++ src/ParametrosInvalidosException.java
@@ -1,3 +1,10 @@
-public class ParametrosInvalidosException {
+
+
+public class ParametrosInvalidosException  extends Exception{
+    
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+    
     
 }
